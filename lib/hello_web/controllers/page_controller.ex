22239d84f2a362bd70f5307da15342726f8ae335
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2023-12-14 13:54:31 -0700

    pheonix-app-from-docs

---
+++ lib/hello_web/controllers/page_controller.ex
@@ -0,0 +1,10 @@
+defmodule HelloWeb.PageController do
+  use HelloWeb, :controller
+
+  plug HelloWeb.Plugs.Locale, "en" when action in [:index]
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
